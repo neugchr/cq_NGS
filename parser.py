748d0d8b5755b37f1dc58c80380beb588da90232
Seminar Bioinformatik <neugchr>
2022-04-20 12:32:34 +0200

    updated parser.py to use argparse

--- parser.py
+++ parser.py
@@ -1,45 +1,50 @@
+import argparse
+
+parser = argparse.ArgumentParser(
+    description="parse a fasta file"
+)
+
+# # positional argument mandatory
+parser.add_argument("infile", type=argparse.FileType("r"), help=("input fasta file"))
+args = parser.parse_args()
+
+
 
 class Sequence:
     def __init__(self, description, sequence):
         self.description = description
         self.sequence = sequence
-        self.suffixes = []
 
     def __str__(self):
         ostr = f"\n--------------------------\n{self.description}\n{self.sequence}\n"
-        for i in self.suffixes:
-            ostr += i+'\n'
         return(ostr)
 
-    def create_suffixes(self):
-        for i in range(len(self.sequence)):
-            self.suffixes.append(self.sequence[i:])
-        self.suffixes.sort()
 
-
-def parse_file(file):
+def parse_file(fh):
     sequences = []
-    with open(file, "r") as fh:
-        description = ''
-        sequence = ''
-        for line in fh:
-            if line[0] == '>':
-                #write the previous sequence
-                if description != '':
-                    sequences.append( Sequence(description, sequence) )
-                description = line
-                sequence = ''
-            else:
-                sequence += line
-        # now write the last sequence as well
-        sequences.append( Sequence(description, sequence) )
+    description = ''
+    sequence = ''
+    for line in fh:
+        if line[0] == '>':
+            #write the previous sequence
+            if description != '':
+                sequences.append( Sequence(description, sequence) )
+            description = line
+            sequence = ''
+        else:
+            sequence += line
+    # now write the last sequence as well
+    sequences.append( Sequence(description, sequence) )
     return(sequences)
 
-seqs = parse_file('unknown.fasta')
-#seqs = parse_file('shortened.fasta')
+
+
+with args.infile as file:
+    seqs = parse_file(file)
+
+
 with open('results.txt', 'w+') as fh:
     for i in range(len(seqs)-1,-1,-1):
-        seqs[i].create_suffixes()
         print(str(seqs[i]))
         fh.write(str(seqs[i]))
         seqs.pop(i)
